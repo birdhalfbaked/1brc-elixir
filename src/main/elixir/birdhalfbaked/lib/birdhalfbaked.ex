f0de4df0a2538f8f3076fe2eb6b0f91c399fe168
Birdy <cmrallen+birdhalfbaked@gmail.com>
2024-10-02 23:31:52 +0200

    Add elixir project to come back to

---
+++ src/main/elixir/birdhalfbaked/lib/birdhalfbaked.ex
@@ -0,0 +1,18 @@
+defmodule Birdhalfbaked do
+  @moduledoc """
+  Documentation for `Birdhalfbaked`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Birdhalfbaked.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ src/main/elixir/birdhalfbaked/test/birdhalfbaked_test.exs
@@ -0,0 +1,8 @@
+defmodule BirdhalfbakedTest do
+  use ExUnit.Case
+  doctest Birdhalfbaked
+
+  test "greets the world" do
+    assert Birdhalfbaked.hello() == :world
+  end
+end
